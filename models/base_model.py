dd23e21effb67ce7118d8e1607e5bd8d4f4cfcff
Nobie120 <n12507400@gmail.com>
2023-12-04 13:14:48 -0800

    re-create an instance with this dictionary representation

--- models/base_model.py
+++ models/base_model.py
@@ -3,23 +3,39 @@
 from datetime import datetime
 import uuid
 
+
 class BaseModel:
     """ defines all common attributes/methods for other classes """
-    def __init__(self):
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_a = datetime.now()
-    def __str__:
+    def __init__(self, *args, **kwargs):
+        """ Instantiation of attributes """
+        if kwargs and kwargs != {}:
+            for key, value in kwargs.items():
+                if key != "__class__":
+                    if key == "created_at" or key == "updated_at":
+                        setattr(self, key,
+                                datetime.strptime(
+                                    value, '%Y-%m-%dT%H:%M:%S.%f'))
+                    else:
+                        setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+
+    def __str__(self):
         """ str representation of the class """
-        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
+        return "[{}] ({}) {}".format(
+                self.__class__.__name__, self.id, self.__dict__)
+
     def save(self):
         """ updates The update time """
-        updated_at = daatetime.now()
+        updated_at = datetime.now()
+
     def to_dict(self):
         """ dictionary containing all
         keys/values of __dict__ of the instance """
-        obj_dict = self.__dict__.copy
-        obj_dict['__class__'] = self.__class__.__name__
+        obj_dict = self.__dict__.copy()
+        obj_dict['__class__'] = type(self).__name__
         obj_dict['created_at'] = self.created_at.isoformat()
         obj_dict['updated_at'] = self.updated_at.isoformat()
         return obj_dict
